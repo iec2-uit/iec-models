f18af418b8d5044cebb4e4233bf93a9fd7692ca0
Nguyễn Văn Hiếu <19521510@gm.uit.edu.vn>
2023-01-12 21:32:28 +0700

    Create cassavaImgClassifier.py

---
+++ cassavaImgClassifier.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+import timm
+
+class CassvaImgClassifier(nn.Module):
+    def __init__(self, model_arch, n_class, pretrained=False):
+        super().__init__()
+        self.model = timm.create_model(model_arch, pretrained=pretrained)
+        
+        self.model.head = nn.Linear(self.model.head.in_features, n_class)
+        '''
+        self.model.classifier = nn.Sequential(
+            nn.Dropout(0.3),
+            #nn.Linear(n_features, hidden_size,bias=True), nn.ELU(),
+            nn.Linear(n_features, n_class, bias=True)
+        )
+        '''
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
